626480eec4a5ee9f192f69c4ecaefb1da8aaca61
Danya Pyat <daniil.pyatiletkin@gmail.com>
2023-11-19 17:50:31 +0100

    first request by user`s query

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print('Hello world!')
-print('So, let`s start')
+import requests
+import json
+
+url = "https://imdb188.p.rapidapi.com/api/v1/searchIMDB"
+
+querystring = {"query": "openheimer"}
+
+headers = {
+    "X-RapidAPI-Key": "b4f10612f7msh1d2976320187102p1c09dcjsn12e5eee2a4d5",
+    "X-RapidAPI-Host": "imdb188.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+with open("sample.json", "w") as outfile:
+    outfile.write(json.dumps(response.json(), indent=2))
