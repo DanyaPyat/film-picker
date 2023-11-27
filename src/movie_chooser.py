9c4d04aa579f0fe6920b3d03ac06bbc1924d34bc
DarknessVoid <61332902+DarknessVoid@users.noreply.github.com>
2023-11-27 21:09:08 +0200

    f git

--- src/movie_chooser.py
+++ src/movie_chooser.py
@@ -16,8 +16,9 @@ class Movie:
 
     def BASE_URL(
         movie_id): return f"https://api.themoviedb.org/3/movie/{movie_id}?language=en-US"
-    def GENRE_URL (): return f"https://api.themoviedb.org/3/discover/movie"
-    def YEAR_URL (): return f"https://api.themoviedb.org/3/discover/movie"
+    GENRE_URL =  f"https://api.themoviedb.org/3/discover/movie"
+    YEAR_URL = f"https://api.themoviedb.org/3/discover/movie"
+    def KEY_URL(query): return f"https://api.themoviedb.org/3/search/movie?query={query}&include_adult=true&language=en-EU&page=1"
 
     params_1 = {
         'api_key': '0dcf953fdad21e9d6cfed187a8769b2d',
@@ -29,11 +30,12 @@ class Movie:
         'with_genres': genre_id,
         'page': randint(1,10)
     }
+
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZGNmOTUzZmRhZDIxZTlkNmNmZWQxODdhODc2OWIyZCIsInN1YiI6IjY1NTk0ODVmYzQ5MDQ4MDExZDBmMWQxMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EXiLARnv71YZZy4FBBmIKY1b5S5X6CIJQLJTAvEpZIw"
     }
-
+ 
 
     async def genres_convert(genre_ids) -> list:
         genre_names = []
@@ -59,7 +61,7 @@ class Movie:
                 
     async def film_picker_genre(genre_id) -> list:
         async with aiohttp.ClientSession() as session:
-            async with session.get(Movie.GENRE_URL(), params=params) as response:
+            async with session.get(Movie.GENRE_URL, params=params) as response:
                 if response.status == 200:
                     data = json.loads(await response.content.read())
                     if 'results' in data and data['results']:
@@ -74,7 +76,7 @@ class Movie:
                 
     async def film_picker_year(release_year)-> list:
         async with aiohttp.ClientSession() as session:
-            async with session.get(Movie.YEAR_URL(),  params = params_1) as response:
+            async with session.get(Movie.YEAR_URL,  params = params_1) as response:
                 if response.status == 200:
                     year = json.loads(await response.content.read())
                     if 'results' in year and year['results']:
@@ -86,18 +88,37 @@ class Movie:
                         return None
                 else:
                     return None
+                
+    async def film_picker_name(query)->list:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(Movie.KEY_URL(query), headers = headers) as response:
+                if response.status == 200:
+                    data = json.loads(await response.content.read())
+                    if 'results' in data and data['results']:
+                        movie = data['results'][0]
+                        genre_ids = movie.get('genre_ids',[0])
+                        genre_names = await Movie.genres_convert(genre_ids)
+                        return [movie['title'], movie['overview'], movie['release_date'], movie['id'], genre_names]
+                    else:
+                        return None
+                else:
+                    return None
+        
                     
-                    
+                   
 
 
 
 async def main():
-    result = await Movie.film_picker_genre(genre_id)
-    film = await Movie.film_pick()
-    year_film = await Movie.film_picker_year(release_year)
-    print(result)
-    print(film)
-    print(year_film)
+    # result = await Movie.film_picker_genre(genre_id)
+    # film = await Movie.film_pick()
+    # year_film = await Movie.film_picker_year(release_year)
+    movie = await Movie.film_picker_name("super")
+    print(movie)
+    # print(result)
+    # print(film)
+    # print(year_film)
+
 
 
 asyncio.get_event_loop().run_until_complete(main())
